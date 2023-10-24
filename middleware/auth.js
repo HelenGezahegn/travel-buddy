3b07b6d0c3ca5c5a0c5e83d9b58c4cd9d874ef59
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-23 22:13:22 -0600

    feat: add authentication (verify tokens)

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    // front-end sets this header, and we're grabbing it throuth this key
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      // 7 based on Bearer length
+      token = token.slice(7, token.length.trimLeft());
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
