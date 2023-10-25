9164a9e14d1d7752fbcdd2a376abe27998fd85af
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-25 00:55:15 -0600

    fix(style): make UserImage circular

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -4,8 +4,7 @@ const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
       <img
-        style={{ objectFit: "cover" }}
-        borderRadius="50%"
+        style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
         alt="user"
