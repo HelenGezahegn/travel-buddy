01c6dfa879d1c320d9744dcf090d00b4ec36fccc
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-24 22:59:34 -0600

    feat: check isAuth to protect home + profile page - if the user is signed in successfully, there'll be a token. If the token exists, then the home and profile page becomes accessible. If no token, then they cannot access these pages. Navigate them back to the login page.

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ import { themeSettings } from "./state/theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
-
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -22,8 +22,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
