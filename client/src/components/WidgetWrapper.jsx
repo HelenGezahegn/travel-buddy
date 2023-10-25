db899db51254129b847c266326dccd486e76affc
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-25 00:08:05 -0600

    fix: typo in WidgetWrapper padding

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -2,8 +2,8 @@ import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
 const WidgetWrapper = styled(Box)(({ theme }) => ({
-  padding: "1.5 rem 1.5rem 0.75rem 1.5rem",
-  backgroundColor: theme.palette.background.alt, 
+  padding: "1.5rem 1.5rem 0.75rem 1.5rem",
+  backgroundColor: theme.palette.background.alt,
   borderRadius: "0.75rem"
 }));
 
