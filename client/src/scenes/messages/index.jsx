16519ae37cc74958bf49374fd761ded00ac7f930
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-28 03:04:01 -0600

    fix: cannot read chat credentials from .env so directly inputting

--- client/src/scenes/messages/index.jsx
+++ client/src/scenes/messages/index.jsx
@@ -3,9 +3,9 @@ import { ChatEngine } from "react-chat-engine";
 const Messages = () => {
   return (
     <ChatEngine
-      projectID={process.env.CHAT_PROJECT_ID}
+      projectID="94ec19d0-978d-43d0-81ed-be570f6539d4"
       userName="Admin"
-      userSecret={process.env.CHAT_PASSWORD}
+      userSecret="admin123"
     />
   );
 };
