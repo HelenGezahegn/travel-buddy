68ee98848bbb34e54315100c1fb382886026bd05
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-28 03:19:35 -0600

    feat: show navbar when on /messages

--- client/src/scenes/messages/index.jsx
+++ client/src/scenes/messages/index.jsx
@@ -1,12 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
+import { Box } from "@mui/material";
+import NavBar from "scenes/navBar";
 
 const Messages = () => {
   return (
-    <ChatEngine
-      projectID={process.env.REACT_APP_CHAT_PROJECT_ID}
-      userName={process.env.REACT_APP_CHAT_USERNAME}
-      userSecret={process.env.REACT_APP_CHAT_PASSWORD}
-    />
+    <Box>
+      <NavBar />
+      <ChatEngine
+        projectID={process.env.REACT_APP_CHAT_PROJECT_ID}
+        userName={process.env.REACT_APP_CHAT_USERNAME}
+        userSecret={process.env.REACT_APP_CHAT_PASSWORD}
+      />
+    </Box>
   );
 };
 
