53a8d6c3b17fa83ca22a4a6b4dbf1fa3a2da0f6e
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-28 03:31:28 -0600

    style: add padding to chat

--- client/src/scenes/messages/index.jsx
+++ client/src/scenes/messages/index.jsx
@@ -6,7 +6,7 @@ const Messages = () => {
   return (
     <Box>
       <NavBar />
-      <Box width="100%" padding="2rem 6%" gap="0.5rem">
+      <Box padding="2rem">
         <ChatEngine
           projectID={process.env.REACT_APP_CHAT_PROJECT_ID}
           userName={process.env.REACT_APP_CHAT_USERNAME}
