435d27b52e8c696c0414d07a8e3997be72f7c942
Helen Gezahegn <gezahegn@ualberta.ca>
2023-11-08 20:51:36 -0700

    fix: render posts once retrieved

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,25 +9,24 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
-  const getPosts = async () => {
-    const response = await fetch(`${baseUrl}/posts`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` }
-    });
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
-  };
-
-  const getUserPosts = async () => {
-    const response = await fetch(`${baseUrl}/posts/${userId}/posts`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` }
-    });
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
-  };
-
   useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch(`${baseUrl}/posts`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
+
+    const getUserPosts = async () => {
+      const response = await fetch(`${baseUrl}/posts/${userId}/posts`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
     if (isProfile) {
       getUserPosts();
     } else {
@@ -37,33 +36,34 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   return (
     <>
-      {posts.map(
-        ({
-          _id,
-          userId,
-          firstName,
-          lastName,
-          description,
-          location,
-          picturePath,
-          userPicturePath,
-          likes,
-          comments
-        }) => (
-          <PostWidget
-            key={_id}
-            postId={_id}
-            postUserId={userId}
-            name={`${firstName} ${lastName}`}
-            description={description}
-            location={location}
-            picturePath={picturePath}
-            userPicturePath={userPicturePath}
-            likes={likes}
-            comments={comments}
-          />
-        )
-      )}
+      {Array.isArray(posts) &&
+        posts.map(
+          ({
+            _id,
+            userId,
+            firstName,
+            lastName,
+            description,
+            location,
+            picturePath,
+            userPicturePath,
+            likes,
+            comments
+          }) => (
+            <PostWidget
+              key={_id}
+              postId={_id}
+              postUserId={userId}
+              name={`${firstName} ${lastName}`}
+              description={description}
+              location={location}
+              picturePath={picturePath}
+              userPicturePath={userPicturePath}
+              likes={likes}
+              comments={comments}
+            />
+          )
+        )}
     </>
   );
 };
